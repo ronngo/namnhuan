0df87ec03a4d521fd4da4f92b2866859c753cd2f
ronngo <70925598+ronngo@users.noreply.github.com>
2020-09-09 20:57:34 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace namnhuan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int year;
+           
+            Console.Write("Nhap nam muon kiem tra : ");
+            year = int.Parse(Console.ReadLine());
+            kiemtraNamNhuan(year);
+            Console.ReadKey();
+        }
+        static int kiemtraNamNhuan(int year)
+        {
+            if (year % 400 == 0 && year % 100 == 0)
+            {
+                Console.Write("Nam {0} la nam nhuan ", year);
+            }
+            else
+            {
+                Console.Write("Nam {0} khong phai la nam nhuan ", year);
+            }
+
+
+            Console.ReadLine();
+            return year;
+        }
+    }
+}
